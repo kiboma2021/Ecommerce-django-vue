0578adf548edb87246cbecc4481349fc0a5d57c1
kiboma <omayioben@gmail.com>
2023-03-12 23:10:53 +0300

    Show and hide mobile contents

--- nextshop_vue/src/App.vue
+++ nextshop_vue/src/App.vue
@@ -6,13 +6,13 @@
           <strong>Nextshop</strong>
         </router-link>
 
-        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
-      <div class="navbar-menu" id="navbar-menu">
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu}">
         <div class="navbar-end">
           <router-link to="/summer" class="navbar-item">Summer</router-link>
           <router-link to="/winter" class="navbar-item">Winter</router-link>
@@ -38,8 +38,18 @@
   <footer class="footer">
     <p class="has-text-centered">Copyright (c) 2021</p>
   </footer>
-  
+
 </template>
+
+<script>
+export default {
+  data(){
+    return {
+      showMobileMenu: false
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 @import '../node_modules/bulma'
