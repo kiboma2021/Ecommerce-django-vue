90989caf1673d6b1f5a3b4268a355cf46d88a1ab
kiboma <omayioben@gmail.com>
2023-03-13 10:10:57 +0300

    Display products

--- nextshop_vue/src/views/HomeView.vue
+++ nextshop_vue/src/views/HomeView.vue
@@ -1,6 +1,35 @@
 <template>
   <div class="home">
-    HOME
+    <section class="hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">
+          Welcome to NextShop
+        </p>
+        <p class="subtitle">
+          The best online store for clothes.
+        </p>
+      </div>
+    </section>
+    <div class="column-is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">
+          Latest Products
+        </h2>
+      </div>
+      <div class="column is-3" v-for="product in latestProducts" :key="product.id">
+        <div class="box">
+          <figure class="image mb-4">
+            <img :src="product.get_thumbnail">            
+          </figure>
+          <h3 class="is-size-4">{{ product.name }}</h3>
+          <p class="is-size-6 has-text-grey">Kshs. {{ product.price }}</p>
+
+          View Details
+        </div>
+
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -9,6 +38,34 @@
 
 export default {
   name: 'HomeView',
-  components: {}
+  components: {},
+  data() {
+    return {
+      latestProducts: [],
+    }
+  },
+  mounted() {
+    //console.log('Mounted...')
+    this.getLatestProducts()
+  },
+  methods: {
+    getLatestProducts() {
+      //console.log('After Mounted...')
+      fetch('http://127.0.0.1:8000/api/v1/latest-products/')
+      .then(res => res.json())
+      .then(data => this.latestProducts = data)
+      .catch(err => console.log(error.message))
+    },
+  }
 }
+
 </script>
+
+<style scoped>
+  .image {
+    margin-top: -1.25rem;
+    margin-left: -1.25rem;
+    margin-right: -1.25rem;
+  }
+
+</style>
