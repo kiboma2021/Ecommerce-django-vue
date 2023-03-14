54e8c3285799d54ac516f9f1bbad264323f39b07
kiboma <omayioben@gmail.com>
2023-03-14 18:12:48 +0300

    Display product detail

--- nextshop_vue/src/views/HomeView.vue
+++ nextshop_vue/src/views/HomeView.vue
@@ -23,8 +23,8 @@
           </figure>
           <h3 class="is-size-4">{{ product.name }}</h3>
           <p class="is-size-6 has-text-grey">Kshs. {{ product.price }}</p>
+          <router-link :to="product.get_absolute_url" class="button is-dark mt-4">View Details</router-link>
 
-          View Details
         </div>
       </div>
     </div>
