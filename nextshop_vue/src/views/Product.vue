54a8a06266d8f58e0377248f8ddc2297a307c653
kiboma <omayioben@gmail.com>
2023-03-15 08:10:46 +0300

    Add product to the cart

--- nextshop_vue/src/views/Product.vue
+++ nextshop_vue/src/views/Product.vue
@@ -17,7 +17,7 @@
                 </div>
 
                 <div class="control">
-                    <a class="button is-dark">Add to cart</a>
+                    <a class="button is-dark" @click="addToCart">Add to cart</a>
                 </div>
 
             </div>
@@ -53,6 +53,16 @@ export default {
             .catch(error => {
                 console.log(error)
             })
+        },
+        addToCart() {
+            if(isNaN(this.quantity) || this.quantity < 1){
+                this.quantity=1
+            }
+            const item = {
+                product: this.product,
+                quantity: this.quantity
+            }
+            this.$store.commit('addToCart', item)
         }
     }
 
