83aa5fd57bf6c37bb13334c104510fe1d059c661
kiboma <omayioben@gmail.com>
2023-03-15 18:09:44 +0300

    Add category serializer

--- nextshop_django/product/serializers.py
+++ nextshop_django/product/serializers.py
@@ -5,3 +5,9 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ('id', 'name', 'get_absolute_url', 'description', 'price', 'get_image', 'get_thumbnail')
+
+class CategorySerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True)
+    class Meta:
+        model = Category
+        fields = ('id', 'name', 'get_absolute_url', 'products')
