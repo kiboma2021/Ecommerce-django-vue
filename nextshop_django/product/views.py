083c9cb13d6a5a4640e070eaf1165e6966aeae55
kiboma <omayioben@gmail.com>
2023-03-15 18:10:52 +0300

    Add queries for CategoryDetail

--- nextshop_django/product/views.py
+++ nextshop_django/product/views.py
@@ -1,7 +1,7 @@
 #from django.shortcuts import render
 from django.http import Http404
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product,Category
+from .serializers import ProductSerializer,CategorySerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -21,4 +21,16 @@ class ProductDetail(APIView):
     def get(self,request,category_slug,product_slug,format=None):
         product = self.get_object(category_slug,product_slug)
         serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+class CategoryDetail(APIView):
+    def get_object(self, category_slug):
+        try:
+            return Category.objects.get(slug=category_slug)
+        except Category.DoesNotExist:
+            raise Http404
+
+    def get(self,request,category_slug,format=None):
+        category = self.get_object(category_slug)
+        serializer = CategorySerializer(category)
         return Response(serializer.data)
