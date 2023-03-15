520379ec46b5a3c4707735fda0214269b36edad7
kiboma <omayioben@gmail.com>
2023-03-15 09:28:59 +0300

    Add loading

--- nextshop_vue/src/App.vue
+++ nextshop_vue/src/App.vue
@@ -31,6 +31,11 @@
     </nav>
   </div>
 
+  <div class="is-loading-bar has-text-centered" :class="{'is-loading':$store.state.isLoading}">
+    <div class="lds-dual-ring"></div>
+
+  </div>
+
   <section class="section">
     <router-view/>
   </section>
@@ -71,6 +76,43 @@ export default {
 </script>
 
 <style lang="scss">
-@import '../node_modules/bulma'
+@import '../node_modules/bulma';
 
+.lds-dual-ring {
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+}
+.lds-dual-ring:after {
+  content: " ";
+  display: block;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border-radius: 50%;
+  border: 6px solid #673AB7;
+  border-color: #673AB7 transparent #673AB7 transparent;
+  animation: lds-dual-ring 1.2s linear infinite;
+}
+
+@keyframes lds-dual-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.is-loading-bar {
+  height: 0;
+  overflow: hidden;
+
+  -webkit-transition: all 0.3s;
+  transform: all 0.3s;
+
+  &.is-loading {
+    height: 80px;
+  }
+}
 </style>
