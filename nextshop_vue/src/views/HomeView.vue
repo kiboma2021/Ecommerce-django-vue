c59786a370195dcdd4dfdd91136fa2141a0d3936
kiboma <omayioben@gmail.com>
2023-03-21 18:52:13 +0300

    Update store categories

--- nextshop_vue/src/views/HomeView.vue
+++ nextshop_vue/src/views/HomeView.vue
@@ -3,17 +3,18 @@
     <div class="columns mb-6">
       <div class="column is-one-fifths has-background-white">
 
-          <router-link to="/summer" class="navbar-item">Clothes</router-link>
-          <router-link to="/winter" class="navbar-item">Electronic</router-link>
-          <router-link to="/winter" class="navbar-item">Funiture</router-link>
-          <router-link to="/winter" class="navbar-item">Beauty</router-link>
-          <router-link to="/winter" class="navbar-item">Automotive</router-link>
+          <router-link to="/summer" class="navbar-item"><i class='fas fa-tshirt' style='color:blue'></i>Clothing</router-link>
+          <router-link to="/winter" class="navbar-item"><i class='fas fa-laptop' style='color:gray'></i>Electronic</router-link>
+          <router-link to="/winter" class="navbar-item"><i class='fas fa-couch' style='color:green'></i>Funiture</router-link>
+          <router-link to="/winter" class="navbar-item"><i class='fas fa-tshirt' style='color:red'></i>Beauty</router-link>
+          <router-link to="/winter" class="navbar-item"><i class='fas fa-car' style='color:olive'></i>Automotive</router-link>
+          <router-link to="/winter" class="navbar-item"><i class='fas fa-pizza-slice' style='color:greenyellow'></i> Food</router-link>
 
       </div>
       <div class="column is-0"></div>
       <div class="column is-three-fifths has-background-grey">is-three-quarter </div>
       <div class="column is-0"></div>
-      <div class="column is-one-fifths has-background-grey">is-three-quarter </div> 
+      <div class="column is-one-fifths has-background-white">is-three-quarter </div> 
  
     </div>
 
